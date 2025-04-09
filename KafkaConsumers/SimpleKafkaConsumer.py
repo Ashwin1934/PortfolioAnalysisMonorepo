edcadee517c2d554b069c0ec782fd19f6cf93d03
Ash D <ashud219@gmail.com>
2025-04-08 20:39:06 -0400

    Add single threaded kafka consumer and spawn tasks to handle each new message as it comes in

--- KafkaConsumers/SimpleKafkaConsumer.py
+++ KafkaConsumers/SimpleKafkaConsumer.py
@@ -19,6 +19,7 @@ def main():
 
         for message in consumer: # blocks and processes messages as they come in
             print(f"Received message: {message.value.decode('utf-8')}")
+            # async io runs single threaded event loop; unlike WebFlux/Project Reactor which has a thread pool running the event loop
             asyncio.run(test_async_function(message=message)) 
 
     except KeyboardInterrupt:
